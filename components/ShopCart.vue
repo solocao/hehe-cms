2e937f95ece1dc201aeb71fc8453222ffc3da3a5
caowei <solocao@126.com>
2018-07-09 15:27:08 +0800

    增加头部和购物车栏目，开发的时候，我的喜欢是先搞大框架，然后再改造，细化

---
+++ components/ShopCart.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="shop-cart">
+    <div class="empty">
+
+    </div>
+    <div class="detail">
+      <div class="item">
+        <div>
+          <img src="http://www.aaebike.com:9090/data/img/2ada81_1513910170835.jpg" alt="">
+        </div>
+        <div class="info">
+          <div class="name">
+            阿迪达斯限量
+          </div>
+          <div class="type">
+            白色
+          </div>
+          <div class="price">
+            <span>4500</span>
+            <span>*1</span>
+          </div>
+        </div>
+
+      </div>
+      <div class="footer">
+        <div>
+          <div class="count">
+            共2件商品
+          </div>
+          <div class="price">
+            <span>合计</span>
+            <span>5009元</span>
+          </div>
+        </div>
+        <div>
+          <button>去购物车</button>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="stylus">
+.shop-cart {
+  z-index: 9999;
+  position: absolute;
+  right: 40px;
+  width: 360px;
+  min-height: 300px;
+  background: white;
+  display: flex;
+  box-shadow: 0 20px 40px rgba(0, 0, 0, 0.15);
+
+  // 空购物车
+  .empty {
+  }
+
+  // 详情
+  .detail {
+    width: 100%;
+
+    .item {
+      height: 100px;
+      padding: 8px;
+      display: flex;
+      align-items: center;
+      border-bottom: 1px solid #d6d6d6;
+
+      img {
+        width: 80px;
+        height: 80px;
+        border-radius: 2px;
+        border: 1px solid #d6d6d6;
+      }
+
+      .info {
+        margin-top: -6px;
+        margin-left: 18px;
+        height: 80px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+
+        .name {
+          font-size: 14px;
+          color: #3e64ea;
+        }
+
+        .type {
+          font-size: 13px;
+          color: #495060;
+        }
+
+        .price {
+          font-size: 15px;
+          font-weight: bold;
+          color: rgb(232, 35, 25);
+        }
+      }
+    }
+
+    .fotter {
+    }
+  }
+
+  // 底部栏目
+  .footer {
+    height: 60px;
+    width: 100%;
+    background: #F7F7F7;
+    display: flex;
+    justify-content: space-around;
+
+    .count {
+    }
+
+    .price {
+    }
+  }
+}
+</style>
---
+++ components/ShopHeader.vue
@@ -0,0 +1,63 @@
+<template>
+  <header class="main-header">
+    <affix relative-element-selector="#example-content">
+      <div class="nav">
+        <ul>
+          <li>
+            <span>首页</span>
+          </li>
+          <li>
+            <span>明星</span>
+          </li>
+        </ul>
+      </div>
+    </affix>
+  </header>
+</template>
+<style lang="stylus">
+@import '../style/var.styl';
+
+.main-header {
+  .info {
+    height: 50px;
+    width: 100%;
+    background: white;
+  }
+
+  .nav {
+    height: 50px;
+    background: white;
+    box-shadow: 0px 2px 5px rgba(158, 158, 158, 0.15);
+
+    &:before {
+      content: '';
+      display: block;
+      height: 2px;
+      background: linear-gradient(to right, #f3005c, #ffde00);
+    }
+
+    &:hover {
+    }
+
+    ul {
+      // 把前面的小圆点点弄没
+      list-style: none;
+      width: content-max;
+      height: 100%;
+      margin: 0 auto;
+      display: flex;
+      align-items: center;
+
+      li {
+        padding-right: 30px;
+
+        span {
+          font-size: 15px;
+          font-style: normal;
+          font-weight: 400;
+        }
+      }
+    }
+  }
+}
+</style>
